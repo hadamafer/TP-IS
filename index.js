097df018ee4bed06932edeb7fa5e509c69f946dc
hadamafer <hadamafer@gmail.com>
2021-06-28 11:16:25 -0300

    cambios en calculator

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ var calculator = require('./app/calculator');
 var a = 3;
 var b = 5;
 
-var v1 = `Add ${a} + ${b} = ${calculator.add(a,b)}`;
-var v2 = `Minus ${a} + ${b} = ${calculator.minus(a,b)}`;
-var v3 = `Multiply ${a} + ${b} = ${calculator.multiply(a,b)}`;
-var v4 = `Divide ${a} + ${b} = ${calculator.divide(a,b)}`;
+var v1 = `Suma ${a} + ${b} = ${calculator.suma(a,b)}`;
+
 
 const express = require('express')
 
@@ -16,6 +14,6 @@ const app = express()
 
 const port = process.env.PORT || 8000
 
-app.get('/', (req,res) => res.send("fer"+v1+ "<br/>" +v2+ "<br/>"+v3 +"<br/>"+v4))
+app.get('/', (req,res) => res.send(v1))
 
 app.listen(port, () => console.log(port))
